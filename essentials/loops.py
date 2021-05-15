474d3938b19f7580bf3a1eaef323fdfb6f176b04
alireza-zahiri <80680631+alirezazahiri@users.noreply.github.com>
2021-05-15 10:52:07 +0430

    loops Done :white_check_mark:

---
+++ essentials/loops.py
@@ -0,0 +1,66 @@
+#TODO: 1. print numbers
+#        """
+#        write a loop to iterate through 1 to 10
+#        print numbers
+#        """
+print('1 ->')
+for i in range(10):
+    print(i, end=' ')
+print()
+for i in range(0, 10, 1):
+    print(i, end=' ')
+print()
+i = 0
+while i < 10:
+    print(i, end=' ')
+    i += 1
+
+#TODO: 2. Print the pattern
+# """
+#     1 
+#     1 2 
+#     1 2 3 
+#     1 2 3 4 
+#     1 2 3 4 5
+# """
+
+print('\n\n2 ->')
+print('for : ')
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end=' ')
+    print()
+
+print('\nwhile : ')
+i = 1
+while i < 6:
+    j = 1
+    while j < i+1:
+        print(j, end=' ')
+        j += 1
+    i += 1
+    print()
+
+#TODO: 3. Print the pattern
+# """
+#     # 
+#     # * 
+#     # * $ 
+#     # * $ @ 
+#     # * $ @ %
+# """
+
+print('\n\n3 ->')
+for i in range(1, 6):
+    for j in range(1, i+1):
+        if j == 1:
+            print('#', end=' ')
+        elif j == 2:
+            print('*', end=' ')
+        elif j == 3:
+            print('$', end=' ')
+        elif j == 4:
+            print('@', end=' ')
+        else:
+            print('%', end=' ')
+    print()
