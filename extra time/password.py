e2349bd52fe48d08d358abb403a20f35fbebcf01
alireza-zahiri <80680631+alirezazahiri@users.noreply.github.com>
2021-05-20 21:03:08 +0430

    quests added :warning:

---
+++ extra time/password.py
@@ -0,0 +1,10 @@
+# import random lib 
+
+# use it to implement a simple random password generator
+
+# our password can contain `a to z` letters `0 to 9` digits and `@ # $ &` characters but no space allowed 
+
+# get the length of password from user 8 <= length of password <= 32
+
+# shuffle it as much as you can :)))
+
---
+++ extra time/swap.py
@@ -0,0 +1,9 @@
+# GET A LIST OF NUMBERS
+
+# SORT THEM USING BUBBLESORT (use shorthand syntax for swapping elements)
+
+# print it to the console
+
+# fix the error ;) 
+
+
